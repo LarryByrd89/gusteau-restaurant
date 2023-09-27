5334b819c43bdf35b9daa9384b3ee6383989c066
mambrocio <mikedbambrocio@gmail.com>
2023-09-26 21:48:10 -0400

    Pulling

---
+++ old/src/structure/Homepage.js
@@ -0,0 +1,19 @@
+import Header from '../Home Components/Header'
+import SectionOne from '../Home Components/SectionOne';
+import SectionTwo from '../Home Components/SectionTwo';
+import SectionThree from '../Home Components/SectionThree';
+import SectionFour from '../Home Components/SectionFour';
+
+function HomePage(){
+return(
+    <>
+    <Header/>
+    <SectionOne/>
+    <SectionTwo/>
+    <SectionThree/>
+    <SectionFour/>
+    </>
+)
+}
+
+export default HomePage;
