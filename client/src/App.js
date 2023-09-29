36f887a24291203872919646352bb48ef22ed4d4
LarryByrd89 <laryn.n99@gmail.com>
2023-09-28 23:29:36 -0400

    added email to nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import AboutPage from "./structure/Aboutpage";
 import Nav from "./structure/Nav";
 import Menu from "./components/Menu";
 import Footer from "./structure/Footer";
+import Contact from "./components/Contact";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from "react";
 
@@ -15,8 +16,8 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/menu" element={<Menu />} />
-          {/* <Route path="/guest-membership" element={GuestMembershipPage} />
-          <Route path="/contact" element={ContactPage} /> */}
+          {/* <Route path="/guest-membership" element={GuestMembershipPage} /> */}
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
         <Footer />
       </div>
