600bbc4c4b2c149408df574d35d70531831da324
Hello <36869500+mambrocio@users.noreply.github.com>
2023-10-02 16:49:11 -0400

    Revert "contact page, membership page, footer nav"

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import AboutPage from "./structure/Aboutpage";
 import Nav from "./structure/Nav";
 import Menu from "./components/Menu";
 import Footer from "./structure/Footer";
-import Contact from "./components/Contact";
-import Membership from "./structure/Membership";
+// import Contact from "./components/Contact";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
@@ -20,8 +19,8 @@ function App() {
             <Route exact path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/menu" element={<Menu />} />
-            <Route path="/guest-membership" element={<Membership />} />
-            <Route path="/contact" element={<Contact/>} />
+            {/* <Route path="/guest-membership" element={GuestMembershipPage} /> */}
+            {/* <Route path="/contact" element={<Contact/>} /> */}
           </Routes>
           <Footer />
         </div>
--- client/src/structure/Footer.js
+++ client/src/structure/Footer.js
@@ -1,41 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer>
-      <div className='container'>
-        <h4>Sign Up For Our Quarterly Newsletter For Updates </h4>
-        <div className="newsletter">
-          <div className='form-div'>
-            <form>
-              <input id='email' type='email' placeholder='Enter Email' required />
-              <br />
-              <input id="submit" type='submit' value="Sign Up" />
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className='footer-nav'>
-        <ul className='footer-links'>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/guest-membership">Guest Membership</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </footer>
+<footer>
+<div className='container'>
+<h4>Sign Up For Our Quarterly Newsletter For Updates </h4>
+    <div className="newsletter">
+        <div className='form-div'>
+        <form>
+            <input id='email' type='email' placeholder='Enter Email' required/>
+            <br/>
+            <input id="submit" type='submit' value="Sign Up"/>
+        </form>
+            </div>
+</div>
+</div>
+</footer>
   );
 }
 
