ab6ef805aaacb4c5befe3b50791038d200179301
LarryByrd89 <laryn.n99@gmail.com>
2023-10-01 20:58:31 -0400

    reservation placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import HomePage from "./structure/Homepage";
 import AboutPage from "./structure/Aboutpage";
 import Membership from "./structure/Membership";
+import Contact from "./components/Contact";
 import Nav from "./structure/Nav";
 import Menu from "./components/Menu";
 import Footer from "./structure/Footer/Footer";
 import Reservation from "./components/Reservation";
-import Contact from "./components/Contact";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
@@ -21,6 +21,8 @@ function App() {
             <Route exact path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/menu" element={<Menu />} />
+            <Route path="/guest-membership" element={<Membership />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="/reservation" element={<Reservation />} />
             <Route path="/guest-membership" element={<Membership />} />
             <Route path="/contact" element={<Contact />} />
